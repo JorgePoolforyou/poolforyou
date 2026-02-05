2ef80366ed03db2ca6dda82be58fdfc4d6f8930d
JorgePoolforyou <info@poolforyou.es>
2026-02-05 09:37:59 +0100

    Add get_db to db.py for Depends

--- app/db.py
+++ app/db.py
@@ -1,6 +1,7 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from typing import Generator
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -15,16 +16,9 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
-# 🔑 ESTO ES LO QUE FALTABA
 Base = declarative_base()
 
-
-from sqlalchemy.orm import sessionmaker
-from app.db import engine
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def get_db():
+def get_db() -> Generator:
     db = SessionLocal()
     try:
         yield db
