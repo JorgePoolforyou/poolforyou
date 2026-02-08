a6dcd90b18e2af1e415e3f34af58c25fef51fa8b
JorgePoolforyou <info@poolforyou.es>
2026-02-08 21:00:45 +0100

    Add CORS origin for poolforyou-frontend-v2

--- app/main.py
+++ app/main.py
@@ -37,6 +37,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://poolforyou-frontend.onrender.com",
+        "https://poolforyouv2.onrender.com/",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
     ],
